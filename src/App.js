ef12e05dc1d20ac268024f7b938018b238c73e01
RazidaChzhan <chrazida@gmail.com>
2019-09-05 23:11:01 +0600

    Добавлено главное меню на странице добавьте свою новость

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MainPage from "./components/mainPage";
-import NewsList from './components/newsList';
-import DetailNews from './components/detailNews';
-import AddNews from './components/addNews';
+import MainPage from "./pages/mainPage";
 import NotFound from './components/NotFound';
+import AddNewsPage from "./pages/addNewsPage";
+import DetailPage from "./pages/detailPage";
 
 
 export default class Menu extends Component {
@@ -13,9 +12,9 @@ export default class Menu extends Component {
             <Router>
                 <Switch>
                   <Route exact path="/" component={MainPage} />
-                  <Route exact path="/allnews" component={NewsList} />
-                  <Route path="/detail" component={DetailNews}/>
-                  <Route path="/add" component={AddNews} />       
+                  <Route exact path="/allnews" component={MainPage} />
+                  <Route path="/detail" component={DetailPage}/>
+                  <Route path="/add" component={AddNewsPage} />       
                   <Route component={NotFound}/>
                 </Switch>
             </Router>
---
+++ src/pages/headerPage.js
@@ -0,0 +1,34 @@
+import React, { Component }from 'react';
+import { NavLink } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header } = Layout;
+
+export default class HeaderPage extends Component {
+
+  render () {
+    return (
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}>
+            <Menu.Item key="1">
+                    <NavLink to={{pathname:'/allnews'}}/>
+                    Все новости 
+                </Menu.Item>
+            <Menu.Item key="2"> 
+              <NavLink to={{pathname:'/add'}}/>
+               Добавьте свою новость  
+            </Menu.Item>
+        </Menu>
+    </Header>
+    );
+  }
+}
+  
+
+
